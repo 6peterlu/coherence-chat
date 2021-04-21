cc48a384b761ca45ed2712b46ac443f1b15a0f02
Peter Lu <6peterlu@gmail.com>
2021-04-21 09:52:47 -0700

    Peter/chatbot nits (#45)
    * ty
    
    * add future scheduling capability
    
    * log login events + patient portal copy
    
    * add logs for andie
    
    * log IP
    
    * blacklist IPs
    
    * log IP again
    
    * add one more IP
    
    * proxyfix
    
    * blacklist the right IPs
    
    * investigating
    
    * fix infinite loading
    
    * print IP again + starting buffer change
    
    * more ip updates + fixing andie
    
    * more IP blacklisting
    
    * syntax
    
    * compartmentalizing
    
    * refactor first pass (#44)
    
    * refactor first pass
    
    * testing infra
    
    * tests
    
    * yml file
    
    * add requirements
    
    * one more req
    
    * update yml
    
    * password
    
    * update dockerhub pass
    
    * add python
    
    * add python image back
    
    * change some urls
    
    * taken @ fixes

---
+++ nlp.py
@@ -0,0 +1,131 @@
+# handler for all regexing and message segmentation (later, more stuff I hope)
+from datetime import date
+from pytz import timezone, utc as pytzutc
+import os
+import string
+import re
+
+# parse timestrings
+import parsedatetime
+
+# fuzzy nlp handling
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
+
+# parse datetime calendar object
+cal = parsedatetime.Calendar()
+
+USER_TIMEZONE = "US/Pacific"
+
+MEDICATION_TAKEN_REGEX = r'(?:\W|^)(taken|take|t)(?:\W|$)'
+TIME_EXTRACTION_REGEX = r'(\d+(?:\:\d+)?)\s*(pm|am|hr|hours|hour|mins|min)?'
+SKIP_REGEX = r'(?:\W|^)(skipped|skipping|skip|s)(?:\W|$)'
+SPECIAL_COMMANDS_REGEX = r'(?:\W|^)(\d+|x)(?:\W|$)'
+
+COMPUTING_PREFIX = "Computing ideal reminder time...done."
+
+RECOGNIZED_ACTIVITIES = {
+    "brunch": {"type": "long", "response": f"{COMPUTING_PREFIX} Have a great brunch! We'll check in later.", "concept": "meal"},
+    "dinner": {"type": "long", "response": f"{COMPUTING_PREFIX} Have a great dinner! We'll check in later.", "concept": "meal"},
+    "lunch": {"type": "long", "response": f"{COMPUTING_PREFIX} Have a great lunch! We'll check in later.", "concept": "meal"},
+    "breakfast": {"type": "long", "response": f"{COMPUTING_PREFIX} Have a great breakfast! We'll check in later.", "concept": "meal"},
+    "eating": {"type": "long", "response": f"{COMPUTING_PREFIX} Enjoy your meal! We'll check in later.", "concept": "meal"},
+    "meeting": {"type": "long", "response": f"{COMPUTING_PREFIX} Have a productive meeting! We'll check in later.", "concept": "work"},
+    "out": {"type": "long", "response": f"{COMPUTING_PREFIX} No problem, we'll check in later.", "concept": "away"},
+    "busy": {"type": "long", "response": f"{COMPUTING_PREFIX} No problem, we'll check in later.", "concept": "work"},
+    "later": {"type": "long", "response": f"{COMPUTING_PREFIX} No problem, we'll check in later.", "concept": "work"},
+    "reading": {"type": "long", "response": f"{COMPUTING_PREFIX} Enjoy your book, we'll check in later.", "concept": "leisure"},
+    "sleeping": {"type": "long", "response": f"{COMPUTING_PREFIX} Sleep well! We'll see you later.", "concept": "leisure"},
+    "golf": {"type": "long", "response": f"{COMPUTING_PREFIX} Have fun out there! We'll see you later.", "concept": "leisure"},
+    "tennis": {"type": "long", "response": f"{COMPUTING_PREFIX} Have fun out there! We'll see you later.", "concept": "leisure"},
+    "swimming": {"type": "long", "response": f"{COMPUTING_PREFIX} Have fun out there! We'll see you later.", "concept": "leisure"},
+    "basketball": {"type": "long", "response": f"{COMPUTING_PREFIX} Have fun out there! We'll see you later.", "concept": "leisure"},
+    "tv": {"type": "long", "response": f"{COMPUTING_PREFIX} Have fun, we'll check in later.", "concept": "leisure"},
+    "working": {"type": "long", "response": f"{COMPUTING_PREFIX} No problem, we'll check in later.", "concept": "work"},
+    "walking": {"type": "short", "response": f"{COMPUTING_PREFIX} Enjoy your walk! We'll check in later.", "concept": "leisure"},
+    "walk": {"type": "short", "response": f"{COMPUTING_PREFIX} Enjoy your walk! We'll check in later.", "concept": "leisure"},
+    "bathroom": {"type": "short", "response": f"{COMPUTING_PREFIX} No problem, we'll check in in a bit.", "concept": "errand"},
+    "run":  {"type": "short", "response": f"{COMPUTING_PREFIX} Have a great run! We'll see you later.", "concept": "leisure"},
+    "running":  {"type": "short", "response": f"{COMPUTING_PREFIX} Have a great run! We'll see you later.", "concept": "leisure"},
+    "basketball":  {"type": "short", "response": f"{COMPUTING_PREFIX} Have fun out there! We'll see you later.", "concept": "leisure"},
+    "shower":  {"type": "short", "response": f"{COMPUTING_PREFIX} No problem, we'll check in later.", "concept": "errand"},
+    "phone": {"type": "short", "response": f"{COMPUTING_PREFIX} Have a great call! We'll check in later.", "concept": "work"},
+    "call": {"type": "short", "response": f"{COMPUTING_PREFIX} Have a great call! We'll check in later.", "concept": "work"},
+}
+
+THANKS_VERSIONS = ["thank", "ty"]
+
+# re-integrate spacy later
+# SPACY_EMBEDDINGS = {token: nlp(token) for token in RECOGNIZED_ACTIVITIES}
+
+
+def get_datetime_obj_from_string(timestring, force=False):
+    datetime_data, parse_status = cal.parseDT(timestring)
+    pacific_time = timezone(USER_TIMEZONE)
+    next_alarm_time = pacific_time.localize(datetime_data.replace(tzinfo=None))
+    if parse_status != 0:
+        return next_alarm_time
+    if force:
+        if len(timestring) <= 2:
+            modified_timestring = f"{timestring}:00"
+            datetime_data, parse_status = cal.parseDT(modified_timestring)
+            next_alarm_time = pacific_time.localize(datetime_data.replace(tzinfo=None))
+            if parse_status != 0:
+                return next_alarm_time
+    return None
+
+def segment_message(raw_message_str):
+    message_segments = []
+    processed_msg = raw_message_str.lower().strip()
+    excited = "!" in processed_msg
+    # remove all punctuation besides : and @
+    punctuation = string.punctuation.replace("@", "").replace(":", "")
+    processed_msg = processed_msg.translate(str.maketrans("", "", punctuation))
+    # remove brackets
+    processed_msg = processed_msg.replace("[", "").replace("]", "")
+    extracted_time = re.findall(TIME_EXTRACTION_REGEX, processed_msg)
+    taken_data = re.findall(MEDICATION_TAKEN_REGEX, processed_msg)
+    print("taken data")
+    print(taken_data)
+    skip_data = re.findall(SKIP_REGEX, processed_msg)
+    special_commands = re.findall(SPECIAL_COMMANDS_REGEX, processed_msg)
+    reconstructed_time = None
+    next_alarm_time = None
+    if extracted_time:
+        reconstructed_time = extracted_time[0][0]
+        if extracted_time[0][1]:
+            reconstructed_time += " " + extracted_time[0][1]
+        next_alarm_time = get_datetime_obj_from_string(reconstructed_time)
+    if taken_data:
+        print(reconstructed_time)
+        print(next_alarm_time)
+        if reconstructed_time and next_alarm_time is None:
+            print("run it again")
+            next_alarm_time = get_datetime_obj_from_string(reconstructed_time, force=True)
+        message_body = {"type": "take", "modifiers": {"emotion": "excited" if excited else "neutral"}}
+        if next_alarm_time is not None:
+            message_body["payload"] = next_alarm_time
+        message_segments.append(message_body)
+    elif skip_data:
+        message_segments.append({"type": "skip"})
+    elif next_alarm_time is not None:
+        message_segments.append({"type": "requested_alarm_time", "payload": next_alarm_time})
+    else:
+        if special_commands:
+            command = special_commands[0]
+            if command not in ['1', '2', '3', 'x']:  # must be a manual minute entry
+                message_segments.append({"type": "delay_minutes", "payload": int(command)})
+            else:
+                message_segments.append({"type": "special", "payload": command})
+        # still process activities if there are special commands
+        for activity in RECOGNIZED_ACTIVITIES:
+            if activity in processed_msg:
+                message_segments.append({"type": "activity", "payload": RECOGNIZED_ACTIVITIES[activity]})
+                break
+    for thanks_version in THANKS_VERSIONS:
+        if thanks_version in processed_msg:
+            message_segments.append({"type": "thanks", "modifiers": {"emotion": "excited" if excited else "neutral"}})
+
+    message_segments = [{**message, "raw": raw_message_str} for message in message_segments]
+    return message_segments
---
+++ test_bot.py
@@ -0,0 +1,221 @@
+from bot import get_take_message
+import pytest
+from unittest import mock
+from datetime import datetime, timedelta
+from pytz import timezone
+
+from models import Event, Dose, Reminder, ManualTakeover
+
+from freezegun import freeze_time
+
+@pytest.fixture
+def dose_record(db_session):
+    dose_obj = Dose(
+        start_hour=11,
+        end_hour=1,
+        start_minute=0,
+        end_minute=0,
+        patient_name="Peter",
+        phone_number="+113604508655",
+        medication_name="test med",
+        active=True
+    )
+    db_session.add(dose_obj)
+    db_session.commit()
+    return dose_obj
+
+@pytest.fixture
+def initial_reminder_record(db_session, dose_record):
+    reminder_obj = Reminder(
+        dose_id=dose_record.id,
+        send_time=datetime(2012, 1, 1, 11),
+        reminder_type="initial"
+    )
+    db_session.add(reminder_obj)
+    db_session.commit()
+
+
+@pytest.fixture
+def manual_takeover_number(db_session):
+    manual_obj = ManualTakeover(phone_number="+113604508655")
+    db_session.add(manual_obj)
+    db_session.commit()
+    return manual_obj
+
+
+# @pytest.fixture
+# def boundary_reminder_record(db_session, dose_record):
+#     reminder_obj = Reminder(
+#         dose_id=dose_record.id,
+#         send_time=datetime(2012, 1, 1, 13),
+#         reminder_type="boundary"
+#     )
+#     db_session.add(reminder_obj)
+#     db_session.commit()
+
+@mock.patch("bot.segment_message")
+@mock.patch("bot.text_fallback")
+def test_not_interpretable(text_fallback_mock, segment_message_mock, client, db_session):
+    segment_message_mock.return_value = []
+    client.post("/bot", query_string={"From": "+13604508655"})
+    assert text_fallback_mock.called
+    all_events = db_session.query(Event).all()
+    assert len(all_events) == 1
+    assert all_events[0].event_type == "not_interpretable"
+
+@mock.patch("bot.client.messages.create")
+@mock.patch("bot.segment_message")
+@mock.patch("bot.get_thanks_message")
+def test_thanks(thanks_message_mock, segment_message_mock, create_messages_mock, client, db_session):
+    segment_message_mock.return_value = [{'modifiers': {'emotion': 'excited'}, 'type': 'thanks', "raw": "T. Thanks!"}]
+    client.post("/bot", query_string={"From": "+13604508655"})
+    assert create_messages_mock.called
+    assert thanks_message_mock.called
+    # assert dose_record.id == db_session.query(Online).all()[0].id
+    all_events = db_session.query(Event).all()
+    assert len(all_events) == 1
+    assert all_events[0].event_type == "conversational"
+
+@mock.patch("bot.client.messages.create")
+@mock.patch("bot.segment_message")
+@mock.patch("bot.get_thanks_message")
+@mock.patch("bot.text_fallback")
+def test_thanks_with_manual_takeover(fallback_mock, thanks_message_mock, segment_message_mock, create_messages_mock, manual_takeover_number, client, db_session):
+    segment_message_mock.return_value = [{'modifiers': {'emotion': 'excited'}, 'type': 'thanks', "raw": "T. Thanks!"}]
+    client.post("/bot", query_string={"From": "+13604508655"})
+    assert not thanks_message_mock.called
+    assert fallback_mock.called
+
+@mock.patch("bot.client.messages.create")
+@mock.patch("bot.segment_message")
+def test_take_without_dose(segment_message_mock, create_messages_mock, client, db_session):
+    segment_message_mock.return_value = [{'type': 'take', 'modifiers': {'emotion': 'neutral'}, "raw": "T"}]
+    client.post("/bot", query_string={"From": "+13604508655"})
+    assert create_messages_mock.called
+    all_events = db_session.query(Event).all()
+    assert len(all_events) == 1
+    assert all_events[0].event_type == "out_of_range"
+
+# within time range of dose
+@freeze_time("2012-01-1 12:00:01")
+@mock.patch("bot.client.messages.create")
+@mock.patch("bot.segment_message")
+@mock.patch("bot.get_take_message")
+def test_take_with_dose(take_message_mock, segment_message_mock, create_messages_mock, client, db_session, dose_record, initial_reminder_record):
+    segment_message_mock.return_value = [{'type': 'take', 'modifiers': {'emotion': 'neutral'}, "raw": "T"}]
+    client.post("/bot", query_string={"From": "+13604508655"})
+    assert create_messages_mock.called
+    assert take_message_mock.called
+    all_events = db_session.query(Event).all()
+    assert len(all_events) == 1
+    assert all_events[0].event_type == "take"
+    assert all_events[0].event_time == datetime(2012, 1, 1, 4, 0, 1)  # match freezegun time
+
+# within time range of dose
+@freeze_time("2012-01-1 12:00:01")
+@mock.patch("bot.client.messages.create")
+@mock.patch("bot.segment_message")
+@mock.patch("bot.get_take_message")
+def test_take_with_dose_and_time(take_message_mock, segment_message_mock, create_messages_mock, client, db_session, dose_record, initial_reminder_record):
+    segment_message_mock.return_value = [{'type': 'take', 'modifiers': {'emotion': 'neutral'}, "raw": "T", "payload": datetime(2012, 1, 1, 8, 0)}]
+    client.post("/bot", query_string={"From": "+13604508655"})
+    assert create_messages_mock.called
+    assert take_message_mock.called
+    all_events = db_session.query(Event).all()
+    assert len(all_events) == 1
+    assert all_events[0].event_type == "take"
+    assert all_events[0].event_time == datetime(2012, 1, 1, 8, 0)  # match input time
+
+# within time range of dose
+@freeze_time("2012-01-1 12:00:01")
+@mock.patch("bot.client.messages.create")
+@mock.patch("bot.segment_message")
+def test_take_with_dose(segment_message_mock, create_messages_mock, client, db_session, dose_record, initial_reminder_record):
+    segment_message_mock.return_value = [{'type': 'skip', 'modifiers': {'emotion': 'neutral'}, 'raw': "S"}]
+    client.post("/bot", query_string={"From": "+13604508655"})
+    assert create_messages_mock.called
+    all_events = db_session.query(Event).all()
+    assert len(all_events) == 1
+    assert all_events[0].event_type == "skip"
+    assert all_events[0].event_time == datetime(2012, 1, 1, 4, 0, 1)  # match freezegun time
+
+# within time range of dose
+@freeze_time("2012-01-1 12:00:01")
+@mock.patch("bot.client.messages.create")
+@mock.patch("bot.segment_message")
+@mock.patch("bot.scheduler.add_job")
+@mock.patch("bot.send_followup_text")
+@mock.patch("bot.get_current_end_date")
+def test_option_1(mock_get_current_end_date, mock_followup_text, add_job_mock, segment_message_mock, create_messages_mock, client, db_session, dose_record, initial_reminder_record):
+    segment_message_mock.return_value = [{'type': 'special', 'payload': '1', "raw": "1"}]
+    mock_get_current_end_date.return_value = timezone("UTC").localize(datetime(2012, 1, 1, 13))
+    client.post("/bot", query_string={"From": "+13604508655"})
+    assert create_messages_mock.called
+    all_events = db_session.query(Event).all()
+    assert len(all_events) == 2
+    assert all_events[0].event_type == "requested_time_delay"
+    assert all_events[0].description == f"{timedelta(minutes=10)}"
+    assert all_events[1].event_type == "reminder_delay"
+    assert all_events[1].description == "delayed to 2012-01-01 04:10:01-08:00"
+    add_job_mock.assert_called_with(f"{dose_record.id}-followup", mock_followup_text, args=[dose_record.id], trigger="date", run_date=timezone("UTC").localize(datetime(2012, 1, 1, 12, 10, 1)), misfire_grace_time=5*60)
+
+
+# within time range of dose
+@freeze_time("2012-01-1 12:00:01")
+@mock.patch("bot.client.messages.create")
+@mock.patch("bot.segment_message")
+@mock.patch("bot.scheduler.add_job")
+@mock.patch("bot.send_followup_text")
+@mock.patch("bot.get_current_end_date")
+def test_option_3_near_boundary(mock_get_current_end_date, mock_followup_text, add_job_mock, segment_message_mock, create_messages_mock, client, db_session, dose_record, initial_reminder_record):
+    segment_message_mock.return_value = [{'type': 'special', 'payload': '3', "raw": "1"}]
+    mock_get_current_end_date.return_value = timezone("UTC").localize(datetime(2012, 1, 1, 13))
+    client.post("/bot", query_string={"From": "+13604508655"})
+    assert create_messages_mock.called
+    all_events = db_session.query(Event).all()
+    assert len(all_events) == 2
+    assert all_events[0].event_type == "requested_time_delay"
+    assert all_events[0].description == f"{timedelta(minutes=60)}"
+    assert all_events[1].event_type == "reminder_delay"
+    assert all_events[1].description == "delayed to 2012-01-01 04:50:00-08:00"
+    add_job_mock.assert_called_with(f"{dose_record.id}-followup", mock_followup_text, args=[dose_record.id], trigger="date", run_date=timezone("UTC").localize(datetime(2012, 1, 1, 12, 50)), misfire_grace_time=5*60)
+
+# within time range of dose
+@freeze_time("2012-01-1 12:00:01")
+@mock.patch("bot.client.messages.create")
+@mock.patch("bot.segment_message")
+@mock.patch("bot.scheduler.add_job")
+@mock.patch("bot.send_followup_text")
+@mock.patch("bot.get_current_end_date")
+def test_1_hr_delay(mock_get_current_end_date, mock_followup_text, add_job_mock, segment_message_mock, create_messages_mock, client, db_session, dose_record, initial_reminder_record):
+    segment_message_mock.return_value = [{'type': 'requested_alarm_time', 'payload': timezone("UTC").localize(datetime(2012, 1, 1, 13)), "raw": "1hr"}]
+    mock_get_current_end_date.return_value = timezone("UTC").localize(datetime(2012, 1, 1, 13))
+    client.post("/bot", query_string={"From": "+13604508655"})
+    assert create_messages_mock.called
+    all_events = db_session.query(Event).all()
+    assert len(all_events) == 1
+    assert all_events[0].event_type == "reminder_delay"
+    assert all_events[0].description == "delayed to 2012-01-01 04:50:00-08:00"
+    add_job_mock.assert_called_with(f"{dose_record.id}-followup", mock_followup_text, args=[dose_record.id], trigger="date", run_date=timezone("UTC").localize(datetime(2012, 1, 1, 12, 50)), misfire_grace_time=5*60)
+
+# within time range of dose
+@freeze_time("2012-01-1 12:00:01")
+@mock.patch("bot.client.messages.create")
+@mock.patch("bot.segment_message")
+@mock.patch("bot.scheduler.add_job")
+@mock.patch("bot.send_followup_text")
+@mock.patch("bot.get_current_end_date")
+@mock.patch("bot.random.randint")
+def test_20_min_delay(mock_randint, mock_get_current_end_date, mock_followup_text, add_job_mock, segment_message_mock, create_messages_mock, client, db_session, dose_record, initial_reminder_record):
+    segment_message_mock.return_value = [{'type': 'activity', 'payload': {'type': 'short', 'response': "Computing ideal reminder time...done. Enjoy your walk! We'll check in later.", 'concept': 'leisure'}, 'raw': 'walking'}]
+    mock_get_current_end_date.return_value = timezone("UTC").localize(datetime(2012, 1, 1, 13))
+    mock_randint.return_value = 23
+    client.post("/bot", query_string={"From": "+13604508655"})
+    assert create_messages_mock.called
+    all_events = db_session.query(Event).all()
+    assert len(all_events) == 2
+    assert all_events[0].event_type == "activity"
+    assert all_events[0].description == "walking"
+    assert all_events[1].event_type == "reminder_delay"
+    assert all_events[1].description == "delayed to 2012-01-01 04:23:01-08:00"
+    add_job_mock.assert_called_with(f"{dose_record.id}-followup", mock_followup_text, args=[dose_record.id], trigger="date", run_date=timezone("UTC").localize(datetime(2012, 1, 1, 12, 23, 1)), misfire_grace_time=5*60)
