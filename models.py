c1242bbdd607977e672b8938a0ebab8d874c657b
Peter Lu <6peterlu@gmail.com>
2021-04-26 22:09:31 -0700

    aware event time

--- models.py
+++ models.py
@@ -367,7 +367,7 @@ class EventLogSchema(Schema):
     id = fields.Integer()
     event_type = fields.String()
     description = fields.String()
-    event_time = fields.DateTime()
+    event_time = fields.AwareDateTime()
     user = fields.Nested(UserSchema(exclude=("dose_windows", "events", "doses")))
     medication = fields.Nested(MedicationSchema(exclude=("dose_windows", "user", "events")))
     dose_window = fields.Nested(DoseWindowSchema(exclude=("user", "events", "medications")))
