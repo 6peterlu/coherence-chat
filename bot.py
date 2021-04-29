26277b8445190df735e9eb394afaf78699c80304
Peter Lu <6peterlu@gmail.com>
2021-04-29 13:17:09 -0700

    fix syntax

--- bot.py
+++ bot.py
@@ -951,9 +951,11 @@ def online_toggle():
 def admin_online_toggle():
     online_status = get_online_status()
     if online_status:  # is online, we need to clear manual takeover on going offline
-        all_users = User.query.filter_by(User.manual_takeover.is_(True)).all()
+        all_users = User.query.filter(User.manual_takeover.is_(False)).all()
         for user in all_users:
             user.manual_takeover = False
+    online_record = Online.query.get(1)
+    online_record.online = not online_status
     db.session.commit()
     return jsonify()
 
