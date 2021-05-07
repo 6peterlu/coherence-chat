b07d4592d995ac8f4e8a73eaa5c9102eabc4a49b
Peter Lu <6peterlu@gmail.com>
2021-05-07 14:33:53 -0700

    whatever

--- web/src/pages/Home.js
+++ web/src/pages/Home.js
@@ -83,7 +83,7 @@ const Home = () => {
                         dayColor = "status-ok";
                     } else if (dayOfMonthData.day_status === "missed") {
                         dayColor = "status-error";
-                    } else if (dayOfMonthData.day_status === "skipped") {
+                    } else if (dayOfMonthData.day_status === "skip") {
                         dayColor = "status-warning";
                     }
                 }
