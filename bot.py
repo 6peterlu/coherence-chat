79c4de00883b8fa26eaa22ab8ec427ff9c504f4d
Peter Lu <6peterlu@gmail.com>
2021-06-07 10:34:45 -0700

    dont double localize

--- bot.py
+++ bot.py
@@ -558,16 +558,12 @@ def update_dose_window_new():
         db.session.commit()
     if dose_window is None:
         return jsonify(), 400
-    user_tz = timezone(dose_window.user.timezone)
-    now = get_time_now()
     start_hour = int(incoming_dw_data["start_hour"])
     start_minute = int(incoming_dw_data["start_minute"])
     end_hour = int(incoming_dw_data["end_hour"])
     end_minute = int(incoming_dw_data["end_minute"])
-    target_start_date = user_tz.localize(datetime(now.year, now.month, now.day, start_hour, start_minute, 0, 0, tzinfo=None)).astimezone(pytzutc)
-    target_end_date = user_tz.localize(datetime(now.year, now.month, now.day, end_hour, end_minute, 0, 0, tzinfo=None)).astimezone(pytzutc)
-    dose_window.edit_window(target_start_date.hour,
-        target_start_date.minute, target_end_date.hour, target_end_date.minute,
+    dose_window.edit_window(start_hour,
+        start_minute, end_hour, end_minute,
         scheduler, send_intro_text_new, send_boundary_text_new
     )
     return jsonify()
